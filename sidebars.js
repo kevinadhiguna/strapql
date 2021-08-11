73934c58592fc7f7731d7d7b8b2bfac9b5221a7e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-08-11 03:13:03 +0000

    feat: add a new page

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3', 'doc4', 'doc5', 'doc6', 'doc7', 'doc8', 'doc9', 'doc10', 'doc11', 'doc12'],
+    Docusaurus: ['doc1', 'doc2', 'doc3', 'doc4', 'doc5', 'doc6', 'doc7', 'doc8', 'doc9', 'doc10', 'doc11', 'doc12', 'doc13'],
     Features: ['mdx'],
   },
 };
