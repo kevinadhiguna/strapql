966d6c008bea69383b9dfe15e3415a76962e2c37
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-08-25 02:28:22 +0000

    refactor: change description

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,7 @@ const features = [
     imageUrl: "img/undraw_docusaurus_mountain.svg",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Strapql is focused on GraphQL queries and mutations.
       </>
     ),
   },
